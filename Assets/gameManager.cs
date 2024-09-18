99da4ce7f1e00fcb620cc6da76344728cd637ca3
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2024-09-18 13:47:56 -0400

    updated spawn

--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -17,13 +17,16 @@ public class gameManager : MonoBehaviour
     WASD playerScript;
     float playerScore = 0;
 
+    [Header("collectible spawn vars")]
     public float spawnInterval = .5f;
     public float spawnTimer = 0f;
+    public Vector4 spawnBox = Vector4.zero; 
     // Start is called before the first frame update
     void Start()
     {
         myPlayer = GameObject.FindGameObjectWithTag("Player");
         playerScript = myPlayer.GetComponent<WASD>();
+        InvokeRepeating("CollectibleSpawn",spawnInterval/2,spawnInterval);
     }
         // Update is called once per frame
     void Update()
@@ -34,12 +37,13 @@ public class gameManager : MonoBehaviour
         //track enemy spawn time here
         spawnTimer += Time.deltaTime;
         //once the interval is hit, trigger an enemy spawn and reset timer
-        if(spawnTimer >= spawnInterval)
-        {
-            spawnTimer = 0f;
-            Instantiate(myCollectible);
-            Debug.Log("enemy spawn");
-        }
+        //if(spawnTimer >= spawnInterval)
+        //{
+        //    spawnTimer = 0f;
+        //    Vector3 targetPos = new Vector3(Random.Range(), Random.Range(), 0);
+        //    Instantiate(myCollectible, );
+        //    Debug.Log("enemy spawn");
+        //}
         //because gameManager has an explicit connection to the player, we
         //can reference the player components, including WASD.cs, and find our score
         playerScore = playerScript.collectedScore;
@@ -49,5 +53,12 @@ public class gameManager : MonoBehaviour
     {
         //add time passed between each physics frame
         myFixedTimer += Time.fixedDeltaTime;
+    }
+
+    void CollectibleSpawn()
+    {
+        Vector3 targetPos = new Vector3(Random.Range(spawnBox.x,spawnBox.y), Random.Range(spawnBox.z,spawnBox.w), 0);
+        Instantiate(myCollectible, targetPos, Quaternion.identity);
+        Debug.Log("collectible spawn");
     }
 }
