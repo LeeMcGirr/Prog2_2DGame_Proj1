4ae2ea601d618ddaa5a95ba8090c4a2e0a88c498
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2024-09-25 16:49:24 -0400

    updated Rot

--- Assets/eyeballTracking.cs
+++ Assets/eyeballTracking.cs
@@ -15,14 +15,28 @@ public class eyeballTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //calculating the distance between the object and player (this is in world space)     
         Vector3 delta = myTarget.transform.position - transform.position;
+        
+        //debug checking the distance from eyeball to player
         Debug.DrawRay(transform.position, delta, Color.yellow);
-        Debug.DrawRay(transform.position, Vector3.up, Color.cyan);
 
-        float angleDelta = Vector3.Angle(delta, Vector3.up);
+        //translate the local up vector of our eyeball into world space coordinates
+        Vector3 localUp = transform.TransformDirection(Vector3.up);
+        Vector3 localForward = transform.TransformDirection(Vector3.forward);
+
+        //debug showing the up vector of the eyeball (this line needs fixing)
+        Debug.DrawRay(transform.position, localUp, Color.cyan);
+        Debug.DrawRay(transform.position, localForward, Color.red);
+
+        //angleDelta find the angle between two vectors, in this case
+        //the local up of the eyeball (pupil) and the vector from player to eyeball
+        float angleDelta = Vector3.Angle(delta, localUp);
         Debug.Log("current angle: " + angleDelta);
 
-        transform.Rotate(Vector3.forward, angleDelta);
+        //rotate by the difference!
+        if (angleDelta > .5)
+        { transform.Rotate(localForward, angleDelta); }
     }
 
 }
